5fba0f19683011662ee6dc98a3c31e6e763ecb37
Mateo <bynpc04@gmail.com>
2024-11-11 18:35:51 -0500

    Proyecto carrito de compras

---
+++ carrito-copia/carrito_de_compras.py
@@ -0,0 +1,20 @@
+class Cliente():
+    pass
+
+class Producto():
+    pass
+
+class CarritoCompras():
+    pass
+
+class Pedido():
+    pass
+
+class Administrador():
+    pass
+
+class MetodoPago():
+    pass
+
+class CuponesDescuentos():
+    pass
